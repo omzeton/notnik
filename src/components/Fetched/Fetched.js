aeb0c43754fff8190f199c9cfb7873261514084b
omzeton <adamr6561@gmail.com>
2019-04-14 15:02:32 +0200

    Convert Fetched to stateless component with React Hooks

--- src/components/Fetched/Fetched.js
+++ src/components/Fetched/Fetched.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Entry from '../Entry/Entry';
 import Noresult from '../Noresult/Noresult';
 import Loader from '../Loader/Loader';
@@ -8,91 +8,87 @@ import * as actionCreators from '../../store/actions';
 
 import './Fetched.css';
 
-class Fetched extends Component {
+const fetched = (props) => {
 
-	state = {
-		fetchStatus: false
-	}
+	const [fetchState, setFetchingState] = useState({ fetchStatus: false });
 
-	onFetch = () => {
-		if(!this.state.fetchStatus) {
-			this.setState({fetchStatus: true});
-			this.props.onFetchSamples(this.props.token);
+	useEffect(() => {
+		if(!fetchState.fetchStatus) {
+			setFetchingState({fetchStatus: true});
+			props.onFetchSamples(props.token);
+			console.log('fetching entries...');
+		}
+	});
+
+	const onFetch = () => {
+		if(!fetchState.fetchStatus) {
+			setFetchingState({fetchStatus: true});
+			props.onFetchSamples(props.token);
 			console.log('fetching entries...');
 		}
 	}
 
-	onReload = () => {
-		if(!this.state.fetchStatus) {
-			this.setState({fetchStatus: true});
-			this.props.onFetchSamples(this.props.token);
-			console.log('reloading...');
-		}
+	let entries;
+	let signStatus = props.isSignedIn;
+
+	if (signStatus) {
+		onFetch();
 	}
 
-	render() {
-		let entries;
-		let signStatus = this.props.isSignedIn;
-
-		if (signStatus) {
-			this.onFetch();
-		}
-
-		if (signStatus) {
-			entries = this.props.error ? <Noresult signed={this.props.isSignedIn} /> : <Loader />;
-		} else {
-			entries = <Noresult signed={this.props.signedIn} />;
-		}
-
-		if ( this.props.import ) {
-			const object = this.props.import;
-
-			let arr = Object.keys(object).map(function(key) {
-			    return object[key];
-			});
-	   		// eslint-disable-next-line
-			entries = arr.map(result => {
-	        // eslint-disable-next-line
-		      if (result.userId == this.props.userId) {
-		          return (<Entry
-		          id={result.id}
-		          key={result.fKey}
-		          header={result.header}
-		          year={result.year}
-		          fKey={result.fKey}
-		          uid={result.userId}
-		          month={result.month}
-		          day={result.day}
-		          text={result.textBody}
-		          img={result.img}/>);
-		      }    			
-			});
-		}
-
-		let eAmount = entries.length,
-		    columns,
-		    str;
-
-		    str = eAmount / 4;
-		    str = str.toString();
-
-		    if ( str.length > 1 ) {
-		      columns = parseInt(str[0], 10);
-		      columns++;
-		    } else {
-		      columns = parseInt(str[0], 10);
-		    }
-
-		let listStyle = {
-		  gridTemplateColumns: `repeat(${columns}, 30em)`
-		}
-
-		return (
-			<div className="Fetched" style={listStyle}>
-			  { entries }
-			</div>
-		);
+	if (signStatus) {
+		entries = props.error ? <Noresult signed={props.isSignedIn} /> : <Loader />;
+	} else {
+		entries = <Noresult signed={props.signedIn} />;
 	}
+
+	if ( props.import ) {
+		const object = props.import;
+
+		let arr = Object.keys(object).map(function(key) {
+		    return object[key];
+		});
+   		// eslint-disable-next-line
+		entries = arr.map(result => {
+        // eslint-disable-next-line
+	      if (result.userId == props.userId) {
+	          return (<Entry
+	          id={result.id}
+	          key={result.fKey}
+	          header={result.header}
+	          year={result.year}
+	          fKey={result.fKey}
+	          uid={result.userId}
+	          month={result.month}
+	          day={result.day}
+	          text={result.textBody}
+	          img={result.img}/>);
+	      }    			
+		});
+	}
+
+	let eAmount = entries.length,
+	    columns,
+	    str;
+
+	    str = eAmount / 4;
+	    str = str.toString();
+
+	    if ( str.length > 1 ) {
+	      columns = parseInt(str[0], 10);
+	      columns++;
+	    } else {
+	      columns = parseInt(str[0], 10);
+	    }
+
+	let listStyle = {
+	  gridTemplateColumns: `repeat(${columns}, 30em)`
+	}
+
+	return (
+		<div className="Fetched" style={listStyle}>
+		  { entries }
+		</div>
+	);
 }
 
 const mapStateToProps = state => {
@@ -111,4 +107,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Fetched);
+export default connect(mapStateToProps, mapDispatchToProps)(fetched);
