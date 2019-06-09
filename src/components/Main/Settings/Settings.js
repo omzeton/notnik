5f6a7199985843e259f08f45d7067b574d54ca30
omzeton <adamr6561@gmail.com>
2019-06-09 15:45:31 +0200

    Add media queries for responsivness

--- src/components/Main/Settings/Settings.js
+++ src/components/Main/Settings/Settings.js
@@ -1,23 +1,25 @@
 import React from "react";
 
-import './Settings.css';
+import "./Settings.css";
 
 const Settings = props => {
-	return (
-		<div className="Settings">
-			<h2>Settings</h2>
-			<div className="Settings__Categories">
-				<p>Reset password</p>
-				<p>Delete account</p>
-				<p>Font size</p>
-				<p>Selection color</p>
-				<p>Menu bar position</p>
-			</div>
-			<form onSubmit={event => props.onLogout(event)}>
-				<input type="submit" value="Log out"/>
-			</form>
-		</div>
-	);
+  return (
+    <div className="Settings__Container">
+      <div className="Settings">
+        <h2>Settings</h2>
+        <div className="Settings__Categories">
+          <p>Reset password</p>
+          <p>Delete account</p>
+          <p>Font size</p>
+          <p>Selection color</p>
+          <p>Menu bar position</p>
+        </div>
+        <form onSubmit={event => props.onLogout(event)}>
+          <input type="submit" value="Log out" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
