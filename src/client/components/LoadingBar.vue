e4188d7f90e4ce475a8864b16c15b063727e3418
Adam Rolczyk <adamr6561@gmail.com>
2021-07-20 21:24:35 +0200

    loading bar text fix

--- src/client/components/LoadingBar.vue
+++ src/client/components/LoadingBar.vue
@@ -1,28 +1,32 @@
 <template>
     <div class="loader" :class="{ 'loader--visible': isLoading }">
-        <h2>{{ message }}</h2>
-        <svg
-            class="loader__svg"
-            version="1.1"
-            id="L4"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            viewBox="0 0 100 100"
-            enable-background="new 0 0 0 0"
-            xml:space="preserve"
-        >
-            <circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
-                <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1" />
-            </circle>
-            <circle fill="#fff" stroke="none" cx="26" cy="50" r="6">
-                <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2" />
-            </circle>
-            <circle fill="#fff" stroke="none" cx="46" cy="50" r="6">
-                <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3" />
-            </circle>
-        </svg>
+        <div class="loader__left-column">
+            <h2>{{ message }}</h2>
+        </div>
+        <div class="loader__right-column">
+            <svg
+                class="loader__svg"
+                version="1.1"
+                id="L4"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 100 100"
+                enable-background="new 0 0 0 0"
+                xml:space="preserve"
+            >
+                <circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
+                    <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1" />
+                </circle>
+                <circle fill="#fff" stroke="none" cx="26" cy="50" r="6">
+                    <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2" />
+                </circle>
+                <circle fill="#fff" stroke="none" cx="46" cy="50" r="6">
+                    <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3" />
+                </circle>
+            </svg>
+        </div>
     </div>
 </template>
 
@@ -51,25 +55,24 @@ export default {
     border-radius: 0.5em;
     font-size: 0.6rem;
     box-sizing: border-box;
-    padding: 1em;
-    padding-right: 6em;
     opacity: 0;
     transition: all 0.3s ease-out;
     z-index: 10;
+    display: grid;
+    grid-template-columns: 1fr 5em;
+    column-gap: 0.5em;
+    padding: 1em;
+    &__left-column,
+    &__right-column {
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
     &__svg {
         position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 50%;
-        right: -1.8em;
-    }
-    &__animation {
-        height: 40%;
-        width: 4em;
-        position: absolute;
-        top: 50%;
-        transform: translateY(-50%);
-        right: 1em;
+        left: 1em;
+        width: 120%;
     }
     &--visible {
         bottom: 3em;
