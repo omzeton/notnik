bec838bfbab3f60d0cab94266240d93d9e870e5a
Adam Rolczyk <adam.rolczyk@syzygy.pl>
2022-01-20 20:06:06 +0100

    Remove redundant types from note module

--- src/client/store/notes.ts
+++ src/client/store/notes.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Dispatch, Commit, ActionTree, GetterTree, MutationTree } from "vuex";
+import { ActionTree, GetterTree, MutationTree } from "vuex";
 
 import { NotesModuleState, Note, Store } from "@/types/store";
 
@@ -9,12 +9,12 @@ const state: NotesModuleState = {
 };
 
 const actions: ActionTree<NotesModuleState, Store> = {
-    async FETCH_ALL_NOTES({ dispatch }: { dispatch: Dispatch }) {
+    async FETCH_ALL_NOTES({ dispatch }) {
         const res = await axios.get("journal/entries");
         if (res.status !== 200 && res.status !== 201) throw new Error("Server error");
         dispatch("UPDATE_NOTES", { notes: res.data.entries });
     },
-    async CREATE_NEW_NOTE({ dispatch }: { dispatch: Dispatch }) {
+    async CREATE_NEW_NOTE({ dispatch }) {
         const res = await axios.post("journal/new");
         if (res.status !== 200 && res.status !== 201) throw new Error("Couldn't create new entry");
         const { entries } = res.data;
@@ -22,47 +22,47 @@ const actions: ActionTree<NotesModuleState, Store> = {
         const newId = entries[entries.length - 1]._id;
         dispatch("SET_ACTIVE_NOTE_ID", { id: newId });
     },
-    async SYNC_CHANGES({ state }: { state: NotesModuleState }) {
+    async SYNC_CHANGES({ state }) {
         const activeNote = state.notes.find(note => note._id === state.activeNoteId);
         if (!activeNote) throw new Error("No active entry found!");
         const res = await axios.post("journal/sync", { entry: activeNote }, { headers: { "Content-Type": "application/json" } });
         if (res.status !== 200 && res.status !== 201) throw new Error("Couldn't sync entry changes");
     },
-    async DELETE_NOTE({ commit }: { commit: Commit }, { id }: { id: Note["_id"] }) {
+    async DELETE_NOTE({ commit }, { id }: { id: Note["_id"] }) {
         commit("deleteNote", id);
         const res = await axios.post("journal/remove-entry", { id }, { headers: { "Content-Type": "application/json" } });
         if (res.status !== 200 && res.status !== 201) throw new Error("Couldn't delete entry!");
     },
-    SET_ACTIVE_NOTE_ID({ commit }: { commit: Commit }, { id }: { id: Note["_id"] }) {
+    SET_ACTIVE_NOTE_ID({ commit }, { id }: { id: Note["_id"] }) {
         commit("setActiveNoteId", id);
     },
-    UPDATE_ACTIVE_NOTE({ commit }: { commit: Commit }, { body }: { body: Note["body"] }) {
+    UPDATE_ACTIVE_NOTE({ commit }, { body }: { body: Note["body"] }) {
         commit("updateActiveNote", { body });
     },
-    UPDATE_NOTES({ commit }: { commit: Commit }, { notes }: { notes: NotesModuleState["notes"] }) {
+    UPDATE_NOTES({ commit }, { notes }: { notes: NotesModuleState["notes"] }) {
         commit("updateNotes", notes);
     },
 };
 
 const getters: GetterTree<NotesModuleState, Store> = {
-    GET_NOTES: (state: NotesModuleState) => state.notes,
-    GET_NOTES_LENGTH: (state: NotesModuleState) => state.notes.length,
+    GET_NOTES: state => state.notes,
+    GET_NOTES_LENGTH: state => state.notes.length,
 };
 
 const mutations: MutationTree<NotesModuleState> = {
-    updateNotes(state: NotesModuleState, payload: NotesModuleState["notes"]) {
+    updateNotes(state, payload: NotesModuleState["notes"]) {
         state.notes = payload;
     },
-    setActiveNoteId(state: NotesModuleState, payload: NotesModuleState["activeNoteId"]) {
+    setActiveNoteId(state, payload: NotesModuleState["activeNoteId"]) {
         state.activeNoteId = payload;
     },
-    updateActiveNote(state: NotesModuleState, payload: Note) {
+    updateActiveNote(state, payload: Note) {
         state.notes = state.notes.map(note => {
             if (note._id === state.activeNoteId) note.body = payload.body;
             return note;
         });
     },
-    deleteNote(state: NotesModuleState, id: Note["_id"]) {
+    deleteNote(state, id: Note["_id"]) {
         state.notes = state.notes.filter(note => note._id !== id);
     },
 };
