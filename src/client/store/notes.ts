b9b3c7de763a92acbeeede5461e3a2065f3e5145
Adam Rolczyk <adam.rolczyk@syzygy.pl>
2022-01-20 19:53:01 +0100

    Improve note typing

--- src/client/store/notes.ts
+++ src/client/store/notes.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
-import { Dispatch, Commit } from "vuex";
+import { Dispatch, Commit, ActionTree, GetterTree, MutationTree } from "vuex";
 
-import { NotesModuleState, Note } from "@/types/store";
+import { NotesModuleState, Note, Store } from "@/types/store";
 
-const state = {
+const state: NotesModuleState = {
     activeNoteId: "",
     notes: [],
 };
 
-const actions = {
+const actions: ActionTree<NotesModuleState, Store> = {
     async FETCH_ALL_NOTES({ dispatch }: { dispatch: Dispatch }) {
         const res = await axios.get("journal/entries");
         if (res.status !== 200 && res.status !== 201) throw new Error("Server error");
@@ -22,48 +22,47 @@ const actions = {
         const newId = entries[entries.length - 1]._id;
         dispatch("SET_ACTIVE_NOTE_ID", { id: newId });
     },
-    async SYNC_CHANGES({ state, dispatch }: { state: NotesModuleState; dispatch: Dispatch }, { notification }: { notification: string }) {
+    async SYNC_CHANGES({ state }: { state: NotesModuleState }) {
         const activeNote = state.notes.find(note => note._id === state.activeNoteId);
         if (!activeNote) throw new Error("No active entry found!");
         const res = await axios.post("journal/sync", { entry: activeNote }, { headers: { "Content-Type": "application/json" } });
         if (res.status !== 200 && res.status !== 201) throw new Error("Couldn't sync entry changes");
-        if (notification) dispatch("ui/DISPLAY_NOTIFICATION", null, { root: true });
     },
-    async DELETE_NOTE({ commit }: { commit: Commit }, { id }: { id: string }) {
+    async DELETE_NOTE({ commit }: { commit: Commit }, { id }: { id: Note["_id"] }) {
         commit("deleteNote", id);
         const res = await axios.post("journal/remove-entry", { id }, { headers: { "Content-Type": "application/json" } });
         if (res.status !== 200 && res.status !== 201) throw new Error("Couldn't delete entry!");
     },
-    SET_ACTIVE_NOTE_ID({ commit }: { commit: Commit }, { id }: { id: string }) {
+    SET_ACTIVE_NOTE_ID({ commit }: { commit: Commit }, { id }: { id: Note["_id"] }) {
         commit("setActiveNoteId", id);
     },
-    UPDATE_ACTIVE_NOTE({ commit }: { commit: Commit }, { body }: { body: string }) {
+    UPDATE_ACTIVE_NOTE({ commit }: { commit: Commit }, { body }: { body: Note["body"] }) {
         commit("updateActiveNote", { body });
     },
-    UPDATE_NOTES({ commit }: { commit: Commit }, { notes }: { notes: Array<Note> }) {
+    UPDATE_NOTES({ commit }: { commit: Commit }, { notes }: { notes: NotesModuleState["notes"] }) {
         commit("updateNotes", notes);
     },
 };
 
-const getters = {
+const getters: GetterTree<NotesModuleState, Store> = {
     GET_NOTES: (state: NotesModuleState) => state.notes,
     GET_NOTES_LENGTH: (state: NotesModuleState) => state.notes.length,
 };
 
-const mutations = {
-    updateNotes(state: NotesModuleState, payload: Array<Note>) {
+const mutations: MutationTree<NotesModuleState> = {
+    updateNotes(state: NotesModuleState, payload: NotesModuleState["notes"]) {
         state.notes = payload;
     },
-    updateActiveNote(state: NotesModuleState, payload: { body: string }) {
+    setActiveNoteId(state: NotesModuleState, payload: NotesModuleState["activeNoteId"]) {
+        state.activeNoteId = payload;
+    },
+    updateActiveNote(state: NotesModuleState, payload: Note) {
         state.notes = state.notes.map(note => {
             if (note._id === state.activeNoteId) note.body = payload.body;
             return note;
         });
     },
-    setActiveNoteId(state: NotesModuleState, payload: string) {
-        state.activeNoteId = payload;
-    },
-    deleteNote(state: NotesModuleState, id: string) {
+    deleteNote(state: NotesModuleState, id: Note["_id"]) {
         state.notes = state.notes.filter(note => note._id !== id);
     },
 };
